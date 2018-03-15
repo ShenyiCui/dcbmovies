6d146384d23aac4237a8cf9b7fc6d8a6d1900438
Shenyi Cui <shenyicui@outlook.com>
2018-03-16 01:31:12 +0800

    Animation, Slideshow
    Some minor CSS fixes to the menu Bar
    
    Fixed the animation problem by migrating animation to Jquery, however still need the inital CSS as the first jQuery never works for some reason
    
    Slide show added. with Discription that shows on hover. Slideshow also halts when hovered

--- js/MAIN.js
+++ js/MAIN.js
@@ -1,20 +1,34 @@
 
 
 function toggleMenu() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.toggle("show");	
+	goOut();
 }
 
+function goOut()
+{
+	$("#myDropdown").animate({left: '0%'},500);
+}
+function goBack()
+{
+	$("#myDropdown").animate({left: '-300%'},500);
+}
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn') && !event.target.matches('.dropdown-content') && !event.target.matches('.dontClose')) {
+    goBack()
+	setTimeout(hideMenu,500);
+  }
+}
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+function hideMenu()
+{
+	var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.toggle('show');
+		  openDropdown.classList.toggle('show');	
       }
     }
-  }
 }
